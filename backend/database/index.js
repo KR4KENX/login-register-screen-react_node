5430087d38b2ef2352f21efcbf56a724d72488b3
adriank64216 <sqat1024@gmail.com>
2023-03-12 18:47:06 +0100

    1.4.1v

--- backend/database/index.js
+++ backend/database/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://sqat1024:asdfgh77@cluster0.kfseani.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://sqat1024:XXXXXXX@cluster0.kfseani.mongodb.net/?retryWrites=true&w=majority')
     .then(() => console.log('Connected to DB'))
     .catch((err) => console.log(err))
